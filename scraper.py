57c0f32dc8b5ea8d4a9ddfd45c7ba12a53a2233e
Your Name <you@example.com>
2022-08-15 01:10:27 +0530

    Initial commit

---
+++ scraper.py
@@ -0,0 +1,111 @@
+#This project can be used in deep learning as we need a lot of images there
+import os
+import time
+import requests
+from selenium import webdriver
+#selenium use to automate the browser searching the urls and all
+
+def fetch_image_urls(query: str, max_links_to_fetch: int, wd: webdriver, sleep_between_interactions: int = 1):
+    def scroll_to_end(wd):
+        wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(sleep_between_interactions)
+
+        # build the google query
+
+    search_url = "https://www.google.com/search?safe=off&site=&tbm=isch&source=hp&q={q}&oq={q}&gs_l=img"
+
+    # load the page
+    wd.get(search_url.format(q=query))
+    #'https://www.google.com/search?safe=off&site=&tbm=isch&source=hp&q=cat&oq=cat&gs_l=img'
+
+    image_urls = set()
+    image_count = 0
+    results_start = 0
+    while image_count < max_links_to_fetch:
+        scroll_to_end(wd)#will scroll down the page then later will find images frim start to the scrolled part
+
+        # get all image thumbnail results
+        thumbnail_results = wd.find_elements_by_css_selector("img.Q4LuWd")#search for the tag value,img is the tag,Tag for
+        #common thumbnails
+        number_results = len(thumbnail_results)
+
+        print(f"Found: {number_results} search results. Extracting links from {results_start}:{number_results}")
+
+        for img in thumbnail_results[results_start:number_results]:
+            # try to click every thumbnail such that we can get the real image behind it
+            try:
+                img.click()#for getting the url
+                time.sleep(sleep_between_interactions)
+            except Exception:
+                continue
+
+            # extract image urls
+            actual_images = wd.find_elements_by_css_selector('img.n3VNCb')
+            for actual_image in actual_images:
+                if actual_image.get_attribute('src') and 'http' in actual_image.get_attribute('src'):
+                    #check if src is there or not and if therecheck whether that src has http string
+                    image_urls.add(actual_image.get_attribute('src'))
+
+            image_count = len(image_urls)
+
+            if len(image_urls) >= max_links_to_fetch:
+                print(f"Found: {len(image_urls)} image links, done!")
+                break
+        else:
+            print("Found:", len(image_urls), "image links, looking for more ...")
+            time.sleep(30)
+            return
+            load_more_button = wd.find_element_by_css_selector(".mye4qd")
+            if load_more_button:
+                wd.execute_script("document.querySelector('.mye4qd').click();")
+
+        # move the result startpoint further down
+        results_start = len(thumbnail_results)
+
+    return image_urls
+
+
+def persist_image(folder_path:str,url:str, counter):
+    try:
+        image_content = requests.get(url).content
+
+    except Exception as e:
+        print(f"ERROR - Could not download {url} - {e}")
+
+    try:
+        f = open(os.path.join(folder_path, 'jpg' + "_" + str(counter) + ".jpg"), 'wb')
+        f.write(image_content)
+        f.close()
+        print(f"SUCCESS - saved {url} - as {folder_path}")
+    except Exception as e:
+        print(f"ERROR - Could not save {url} - {e}")
+
+
+def search_and_download(search_term: str, driver_path: str, target_path='./images', number_images=10):
+    target_folder = os.path.join(target_path, '_'.join(search_term.lower().split(' ')))
+
+    if not os.path.exists(target_folder):
+        os.makedirs(target_folder)
+
+    with webdriver.Chrome(executable_path=driver_path) as wd:#will open a browser
+        res = fetch_image_urls(search_term, number_images, wd=wd, sleep_between_interactions=0.5)
+
+    counter = 0
+    for elem in res:
+        persist_image(target_folder, elem, counter)
+        counter += 1
+
+#Version 96.0.4664.45
+# How to execute this code
+# Step 1 : pip install selenium. pillow, requests
+# Step 2 : make sure you have chrome installed on your machine
+# Step 3 : Check your chrome version ( go to three dot then help then about google chrome )
+# Step 4 : Download the same chrome driver from here  " https://chromedriver.storage.googleapis.com/index.html "
+# Step 5 : put it inside the same folder of this code
+
+
+DRIVER_PATH = './chromedriver.exe'
+search_term = 'cat'
+# num of images you can pass it from here  by default it's 10 if you are not passing
+number_images = 10
+search_and_download(search_term=search_term, driver_path=DRIVER_PATH)
